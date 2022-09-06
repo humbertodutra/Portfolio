b7babc748380c4b506c751a45e9d56bc24d249a4
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-09-06 14:02:58 -0300

    footer

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -31,7 +31,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>made with &hearts; by <a href={'https://humbertodutra.git.io'}>humberto</a></p>
+                  <p>made with &hearts;, and react by <a href={'https://humbertodutra.git.io'}>humberto</a></p>
                   <p>&copy; 2022</p>
                </Box>
             </Grid>
