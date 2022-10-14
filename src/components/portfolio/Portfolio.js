2c3af35aea01c75cccb0fcd2e8028cc2a58f59f0
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-10-14 19:22:31 -0300

    adding a project

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,11 +4,12 @@ import {Box, Grid} from "@mui/material";
 import {info} from "../../info/Info";
 
 export default function Portfolio() {
-    return (
-        <Box>
-            <Grid container display={'flex'} justifyContent={'center'}>
+    return (       
+       <Box marginTop={10}>
+        <h2>Frontend</h2>
+            <Grid container display={'flex'} justifyContent={'center'} marginTop={6}>
                 {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
+                   <Grid item xs={2} md={4} key={index}>
                        <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
                    </Grid>
                 ))}
--- src/info/Info.js
+++ src/info/Info.js
@@ -115,9 +115,9 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            title: "Project FootballClub Front/Back",
+            live: "https://tfcfrontend.herokuapp.com/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://tfcfrontend.herokuapp.com/", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
